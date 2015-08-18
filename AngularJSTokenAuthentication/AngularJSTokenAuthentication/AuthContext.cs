9cf78edc8b354be256435fde46edca3a8286dfe6
khanasif1 <khanasif1@gmail.com>
2015-08-18 16:59:12 +1000

    AngularJs Token Authentication
    AngularJs Token Authentication

---
+++ AngularJSTokenAuthentication/AngularJSTokenAuthentication/AuthContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSTokenAuthentication
+{
+    public class AuthContext:IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        { 
+        }
+    }
+}
